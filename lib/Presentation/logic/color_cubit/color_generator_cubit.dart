1489798df9f7c227376f4478dccf83ff27d75839
islam <mobile@queentechsolutions.net>
2023-03-29 01:47:16 +0200

    update color_generator_cubit ( handle color picker logic)

--- lib/Presentation/logic/color_cubit/color_generator_cubit.dart
+++ lib/Presentation/logic/color_cubit/color_generator_cubit.dart
@@ -6,19 +6,44 @@ import 'package:test_task/Presentation/logic/color_cubit/color_generator_states.
 
 /// Color Generator Bloc Cubit
 class ColorGeneratorCubit extends Cubit<ColorGeneratorStates> {
-
   /// Generated Color value
-  Color generatedColor = Colors.white;
+  Color backgroundColor = Colors.white;
+
+  ///list of colors for color picker
+  List<Color> pickedColors = [];
+
+  ///color picker list length
+  final int _colorLength = 4;
 
   /// Color Generator Constructor calling super
   ColorGeneratorCubit() : super(ColorInitState());
 
-  /// Color Generator function
-  void generateColor() {
-    ///get random integer with color primaries length
-    generatedColor =
-        Colors.primaries[Random().nextInt(Colors.primaries.length)];
+  ///  generate background color function
+  void updateBackgroundColor({Color? selectedColor}) {
+    backgroundColor = selectedColor ?? _generateColor();
+
     ///emit ColorChangeState to change the view
     emit(ColorChangeState());
+  }
+
+  /// Color Generator function
+  Color _generateColor() {
+    ///get random integer with color primaries length
+    return Colors.primaries[Random().nextInt(Colors.primaries.length)];
+  }
+
+  ///fill color picker list function
+  void fillColorPickerList() {
+    for (int i = 0; i < _colorLength; i++) {
+      pickedColors.add(_generateColor());
+    }
+  }
+
+  ///update color picker list function
+  void updateColorPickerList() {
+    pickedColors.clear();
+    for (int i = 0; i < _colorLength; i++) {
+      pickedColors.add(_generateColor());
+    }
   }
 }
