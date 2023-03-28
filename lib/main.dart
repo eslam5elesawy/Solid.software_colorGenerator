d3db54ae225da1de0290426744c2bcc3d766b325
islam <mobile@queentechsolutions.net>
2023-03-28 23:12:50 +0200

    separate main material app file and remove useless MyApp class

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:test_task/Presentation/screen/color_generator_screen.dart';
+import 'package:test_task/main_material_app.dart';
 
 void main() async {
-  runApp(const MyApp());
+  runApp(const MainMaterialApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
-  @override
-  Widget build(BuildContext context) {
-    return const HomeMaterialApp();
-  }
-}
-
-class HomeMaterialApp extends StatelessWidget {
-  const HomeMaterialApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'App Title',
-      theme: ThemeData(
-        primaryColor: Colors.blueAccent,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const ColorGeneratorScreen()
-    );
-  }
-}
