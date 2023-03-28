223d71533280a7f9ca96e96eff484a41ec180156
islam <mobile@queentechsolutions.net>
2023-03-29 01:45:48 +0200

    add AppConstance file contain Constance values

---
+++ lib/app_constance.dart
@@ -0,0 +1,22 @@
+///class for Constance values
+class AppConstance {
+
+  /// small page padding
+  static const double smallPagePadding = 8;
+
+  /// page padding
+  static const double normalPagePadding = 16;
+
+
+  /// small border Radius
+  static const double smallBorderRadius = 4;
+
+  /// page padding
+  static const double normalBorderRadius = 8;
+
+  /// color picker container radius
+  static const double colorRadius = 45;
+
+  /// color picker bottom sheet height
+  static const double bottomSheetHeight = 150;
+}
