afd1e0fa2f0b0c0e12e54e7404c1c48e3632d4bf
islam <mobile@queentechsolutions.net>
2023-03-29 01:49:20 +0200

    handle color picker UI

--- lib/Presentation/screen/color_generator_screen.dart
+++ lib/Presentation/screen/color_generator_screen.dart
@@ -2,32 +2,73 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:test_task/Presentation/logic/color_cubit/color_generator_cubit.dart';
 import 'package:test_task/Presentation/logic/color_cubit/color_generator_states.dart';
+import 'package:test_task/Presentation/widget/color_picker_widget.dart';
+import 'package:test_task/app_constance.dart';
 
 /// clickable screen to display text "Hello there" in the middle of the screen
-class ColorGeneratorScreen extends StatelessWidget {
-
+class ColorGeneratorScreen extends StatefulWidget {
   /// Constructor calling super
   const ColorGeneratorScreen({Key? key}) : super(key: key);
 
   @override
+  State<ColorGeneratorScreen> createState() => _ColorGeneratorScreenState();
+}
+
+class _ColorGeneratorScreenState extends State<ColorGeneratorScreen> {
+  @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<ColorGeneratorCubit>(context).fillColorPickerList();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocProvider<ColorGeneratorCubit>(
-        create:(context) => ColorGeneratorCubit(),
-        child: BlocBuilder<ColorGeneratorCubit,ColorGeneratorStates>(
-          builder: (colorCtx, colorState) => DecoratedBox(
-            decoration: BoxDecoration(
-                color: colorCtx.read<ColorGeneratorCubit>().generatedColor,
-            ),
-            child: InkWell(
-              onTap: () {
-                ///calling function to change background color
-                colorCtx.read<ColorGeneratorCubit>().generateColor();
-              },
-              ///central 'Hello there' text
-              child: const Center(
-                child: Text('Hello there'),
-              ),
+      body: BlocBuilder<ColorGeneratorCubit, ColorGeneratorStates>(
+        builder: (colorCtx, colorState) => DecoratedBox(
+          decoration: BoxDecoration(
+            color: colorCtx.read<ColorGeneratorCubit>().backgroundColor,
+          ),
+          child: InkWell(
+            onTap: () {
+              ///calling function to change background color
+              colorCtx.read<ColorGeneratorCubit>().updateBackgroundColor();
+            },
+            onLongPress: () {
+              showModalBottomSheet<Widget>(
+                context: context,
+                backgroundColor: Colors.transparent,
+                builder: (_) => Container(
+                  height: AppConstance.bottomSheetHeight,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(
+                      AppConstance.normalBorderRadius,
+                    ),
+                  ),
+                  child: ColorPickerWidget(
+                    pickedColors:
+                        colorCtx.read<ColorGeneratorCubit>().pickedColors,
+                    onColorTab: (selectedColor) {
+                      ///calling function to change background color
+                      colorCtx
+                          .read<ColorGeneratorCubit>()
+                          .updateBackgroundColor(
+                            selectedColor: selectedColor,
+                          );
+
+                      ///calling update pick color list
+                      colorCtx
+                          .read<ColorGeneratorCubit>()
+                          .updateColorPickerList();
+                      Navigator.of(_).pop();
+                    },
+                  ),
+                ),
+              );
+            },
+            child: const Center(
+              child: Text('Hello there'),
             ),
           ),
         ),
